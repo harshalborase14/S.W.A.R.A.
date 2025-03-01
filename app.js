b0af950971608bcaa3927d949976739d3edc48a9
harshalborase14 <htborase@gmail.com>
2025-03-01 19:23:24 +0530

    Update app.js

--- app.js
+++ app.js
@@ -618,7 +618,8 @@ function readOut(message) {
     //diff voices
     const allVoices = speechSynthesis.getVoices();
     speech.text = message;
-    speech.voice = allVoices[167]; //hindi swara
+    speech.voice = allVoices[10];
+    //speech.voice = allVoices[167]; //hindi swara
     // speech.voice = allVoices[211]; //marathi arohi
     speech.volume = 1;
     speech.rate = 1.1;
